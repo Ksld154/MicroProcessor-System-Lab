0e404554bacef520e0831a6585059342f902468c
Alex Lin <ardt4113c@gmail.com>
2019-10-05 01:31:52 +0800

    2-1 pass example test_case

--- Lab2/P1.s
+++ Lab2/P1.s
@@ -5,19 +5,23 @@
 
 .data
     user_stack_bottom: .zero 128
+    .equ stk_size, .-user_stack_bottom
     expr_result: .word 0
 
 .text
     .global main
-    postfix_expr: .asciz "-100 10 20 + - 10 +" //ans: -100
+    .equ negative, -1
+    postfix_expr: .asciz "-100 10 20 + - 10 +" //ans: -120
+    @ postfix_expr: .asciz "3 5 4 - +" //ans: -4
     
     main:
         // TODO: Setup stack pointer to end of user_stack and calculate the
         // expression using PUSH, POP operators, and store the result into expr_result
 
-        LDR  R0, =postfix_expr        // save postfix_expr's address into R0
+        LDR  R0, =postfix_expr            // save postfix_expr's address into R0
+        LDR  R9, =expr_result
         LDR  SP, =user_stack_bottom   
-        ADDS SP, #128                 // SP(R13): The highest memory address of the user_stack 
+        ADDS SP, stk_size                 // SP(R13): The highest memory address of the user_stack 
         B evaluate_postfix
 
 
@@ -30,22 +34,22 @@
         // R3: expr[i]
 
         MOVS R2, #0
+        MOVS R10, #-1
         
         eval_ScanLoop:                  // for(i = 0; i < strlen(expr); i++)
-            CMP  R2, R1                 //     if(i == strlen(expr)) exit_loop
-            BEQ  eval_Escape
+            CMP  R2, R1                 //     if(i >= strlen(expr)) exit_loop
+            BGE  show_result
 
             LDRB R3, [R0, R2]           //     R3: expr[i]
 
-            CMP  R3, #32                //     if (s[i] == ' ' || s[i] == ',')
-            BEQ　eval_isSpace           //         then do nothing, i++
+            CMP  R3, #32                //     if (s[i] == ' ')
+            BEQ  eval_isSpace           //         then do nothing, i++
             CMP  R3, #48                //     else if (s[i] is digit)
             BGT  eval_isDigit           //         get the integer value
             CMP  R3, #43                //     else if (s[i] == '+')
             BEQ  eval_plus              //         do add operation
             CMP  R3, #45                //     else if (s[i] == '-')
             BEQ  eval_minus             //         NOT SURE it's (1)sign bit or (2) subtract operation
-
 
             eval_isSpace:
                 ADDS R2, #1
@@ -62,23 +66,24 @@
                 B eval_ScanLoop
             
             eval_minus:
+                @ expr[i] == '-' 
                 @ Might be (1)sign bit or (2) MINUS operation
                 ADDS R2, R2, #1
                 LDRB R4, [R0, R2]       // R4: expr[i+1]
                 SUBS R2, #1
 
-                CMP  R4, #32            // next byte after '-' is space
+                CMP  R4, #32            // expr[i+1] == ' '
                 BEQ  operation_minus    //      do MINUS operation
-                CMP  R4, #0             // '-' is the last byte of expr
+                CMP  R4, #0             // expr[i+1] == '\0'
                 BEQ  operation_minus    //      do MINUS operation
 
-                MOVS R7, #1             // sign bit is '-'
-                // SUBS R2, #1             // send both SIGNBIT and VALUE to atoi
+                MOVS R7, #1             // sign bit is 1(negative)
+                ADDS R2, #1
                 BL atoi
                 CMP  R2, R1             // check expr[i] doesn't exceed it's boundary
                 BLT  eval_ScanLoop      
 
-
+        
 
 
     strlen:
@@ -90,7 +95,7 @@
 
         strlen_Loop:              // for(i = 0; arr[i]!='\0'; i++)
             LDRB R2, [R0, R1]     //    R2: arr[i]
-            CMP  R2, 0            //    if(arr[i] == '\0') 
+            CMP  R2, 0            //    if(arr[i] == -*/0') 
             BEQ  strlen_Escape    //        exit for_loop
             ADDS R1, #1           //    i++
             B strlen_Loop
@@ -107,15 +112,17 @@
     
         // R6: converted_integer
         // R7: sign bit
-
-        MOVS R6, #0
+        // R8: #10
+        MOV  R6, #0
+        MOV  R8, #10
+        
 
         atoi_Loop:                  // for(i = 0; expr[i]!='\0'; i++)
-            LDRB R3, [R0, R2]       //    R3: exor[i]
+            LDRB R3, [R0, R2]       //    R3: expr[i]
             CMP  R3, #32            //    if(expr[i] == ' ') 
             BEQ  atoi_Return        //        exit for_loop
-            
-            MULS R6, #10            // R6 *= 10
+
+            MUL  R6, R8             // R6 *= 10
             SUBS R3, #48            
             ADDS R6, R3             // R6 += input_str[i] - '0';
             
@@ -123,14 +130,44 @@
             B atoi_Loop
         
         atoi_Return:
+            CMP  R7, #1
+            BNE  atoi_ReturnPositive            
+            
+            SUBS R7, #2
             MULS R6, R7             // R6 *= signBit
+            PUSH {R6}
             BX LR
-
+        
+        atoi_ReturnPositive:
+            PUSH {R6}
+            BX LR
    
     
     operation_plus:
-    
+        // R4: Operand 1 => result
+        // R5: Operand 2 
+
+        // 1. pop 2 operands from stack, and add them together
+        // 2. push the result back to stack
+
+        POP  {R4}
+        POP  {R5}
+        ADDS R4, R5
+        PUSH {R4}
+        BX LR
+
     operation_minus:
+        POP  {R4}
+        POP  {R5}
+        SUBS R5, R4
+        PUSH {R5}
+        ADDS R2, #1
+        B eval_ScanLoop
+
+    show_result:
+        POP  {R4}
+        STRB R4, [R9]
+        B program_end
 
     program_end:
         B program_end
