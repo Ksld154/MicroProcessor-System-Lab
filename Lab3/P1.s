de9ffac1361da6263c03a6ffa0a37715ed67b817
Alex Lin <ardt4113c@gmail.com>
2019-10-20 03:57:36 +0800

    3-1 report done

--- Lab3/P1.s
+++ Lab3/P1.s
@@ -32,28 +32,28 @@ Loop:
 //R0 = Address of leds
 //R8 = output
 
-@ R1: index of current lighten leds
-@ R2: index of next leds
+// R1: index of current lighten leds
+// R2: index of next leds
     
     @ A5 -> A8 (0001(1) -> 0011(3) -> 0110(6) -> 1100(12) -> 1000(8))
     GOLEFT: 
         LDR     R1, [R0]
         LSL     R2, R1, #1
-        CMP     R1, #1      @ when 0001(1) -> 0011(3)
+        CMP     R1, #1     // when 0001(1) -> 0011(3)
         IT		EQ
         ADDEQ   R2, #1
         
-        AND     R2, 0xF     @ Mask upper bits to be all 0s, because when 01100(12) -> 11000, 
-                            @ we need to discard the highest bit to make it become 1000(8)
-        STRB    R2, [R0]    @ Save the index of next led, for later iteration
-        BL      DisplayLED  @ Lighten next led
+        AND     R2, 0xF     // Mask upper bits to be all 0s, because when 01100(12) -> 11000, 
+                            // we need to discard the highest bit to make it become 1000(8)
+        STRB    R2, [R0]    // Save the index of next led, for later iteration
+        BL      DisplayLED  // Lighten next led
         LDR     R3, =ONE
-        BL      Delay       @ Delay one second
+        BL      Delay       // Delay one second
 
-        CMP     R2, 0x8     @ if leftmost light is on, then go RIGHT
+        CMP     R2, 0x8     // if leftmost light is on, then go RIGHT
         BNE     GOLEFT
     
-    @ A8 -> A5
+    // A8 -> A5
     GORIGHT:
         LDR     R1, [R0]
         LSR     R2, R1, #1
@@ -75,22 +75,22 @@ Loop:
 GPIO_init:
     //TODO: Initial LED GPIO pins as output
 
-    @ Enable AHB clock for using GPIO bus   
-    MOVS    R0, 0x1           @ 0x1 means using GPIO_A 
+    // Enable AHB clock for using GPIO bus   
+    MOVS    R0, 0x1           // 0x1 means using GPIO_A 
     LDR     R1, =RCC_AHB2ENR
     STR     R0, [R1]
 
     
-    MOVS    R0, 0x15400      @ 0x15400 means MODE5~MODE8 = 0b01 (output mode)
+    MOVS    R0, 0x15400      // 0x15400 means MODE5~MODE8 = 0b01 (output mode)
     LDR     R1, =GPIOA_MODER
     LDR     R2, [R1]
-    AND     R2, 0xFFFC03FF   @ mask MODE5~MODE8 = 0b00 first
+    AND     R2, 0xFFFC03FF   // mask MODE5~MODE8 = 0b00 first
     ORRS    R2, R2, R0
-    STR     R2, [R1]         @ set MODE5~MODE8 to output mode, and write to IO memory
+    STR     R2, [R1]         // set MODE5~MODE8 to output mode, and write to IO memory
 
     MOVS    R0, 0x2A800      
     LDR     R1, =GPIOA_OSPEEDR
-    STRH    R0, [R1]         @ high speed mode for OSPEED5~OSPEED8
+    STRH    R0, [R1]         // high speed mode for OSPEED5~OSPEED8
 
     LDR     R8, =GPIOA_ODR
 
@@ -99,12 +99,12 @@ GPIO_init:
 DisplayLED:
     //TODO: Display LED by leds
     
-    @ R7: Index of led
-    @ R8: Address of GPIOA's output register
+    // R7: Current lighten leds
+    // R8: Address of GPIOA's output register
 
-    LDR     R7, [R0]   @ R7  = either 1, 2, 4, 8
-    LSL     R7, #5     @ R7 *= 32 because we are using A5~A8
-    MVN     R7, R7     @ R7 =  ~R7 because the led is "ACTIVE LOW", lighten when it's output register is 0
+    LDR     R7, [R0]   
+    LSL     R7, #5     // R7 *= 32 because we are using A5~A8
+    MVN     R7, R7     // R7 =  ~R7 because the led is "ACTIVE LOW", lighten when it's output register is 0
     STRH    R7, [R8]
     BX      LR
 
