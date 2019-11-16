4900aca44124de3baf6bcdcc9a918c2c3d4b2923
Nicholas Li <43367240+rti56kt@users.noreply.github.com>
2019-11-16 17:08:30 +0800

    P3 done

--- Lab6/P3.c
+++ Lab6/P3.c
@@ -6,19 +6,19 @@
 
 extern void GPIO_init();
 
-// float key_value[PAD_SIZE][PAD_SIZE] = {
-//     {261.6,  293.7,  329.6,  0},
-//     {349.2,  392.0,  440.0,  0},
-//     {493.9,  523.3,      0,  0},
-//     {    0,      0,      0,  0}
-// };
-
 int key_value[PAD_SIZE][PAD_SIZE] = {
-    {261,  293,  329,  0},
-    {349,  392,  440,  0},
-    {493,  523,    0,  0},
-    {  0,    0,    0,  0}
+    {2616,  2937,  3296,  0},
+    {3492,  3920,  4400,  0},
+    {4939,  5233,     0,  0},
+    {   0,     0,     0,  0}
 };
+
+// int key_value[PAD_SIZE][PAD_SIZE] = {
+//     {261,  293,  329,  0},
+//     {349,  392,  440,  0},
+//     {493,  523,    0,  0},
+//     {  0,    0,    0,  0}
+// };
 
 void keypad_init(){
     // using GPIO_A, GPIO_B, GPIO_C
@@ -26,23 +26,26 @@ void keypad_init(){
 
     // PB5-PB8: input mode
     // Set PB5-PB8 to 0b00 00 00 00 (input mode)
-    GPIOB->MODER = 0xFFFC03FF;
+    GPIOB->MODER &= 0xFFFC03FF;
+    GPIOB->MODER |= 0x80;
 
     // Set PB5-PB8 to 0b01 01 01 01 (pull-down)
-    GPIOB->PUPDR = 0xFFFC03FF;
-    GPIOB->PUPDR = GPIOB->PUPDR | 0x2A800;
+    GPIOB->PUPDR &= 0xFFFC03FF;
+    GPIOB->PUPDR |= 0x2A800;
 
     // Set PB5-PB8 to 0b10 10 10 10 (high speed)
-    GPIOB->OSPEEDR = 0xFFF2A8FF;
+    GPIOB->OSPEEDR &= 0xFFFC03FF;
+    GPIOB->OSPEEDR |= 0x2A800;
 
 
     // PC5-PC8: output mode
     // Set PC5-PC8 to 0b01 01 01 01 (output mode)
-    GPIOC->MODER = 0xFFFC03FF;
-    GPIOC->MODER = GPIOC->MODER | 0x15400;
+    GPIOC->MODER &= 0xFFFC03FF;
+    GPIOC->MODER |= 0x15400;
 
     // Set PC5-PC8 to 0b10 10 10 10 (high speed)
-    GPIOC->OSPEEDR = 0xFFF2A8FF;
+    GPIOC->OSPEEDR &= 0xFFFC03FF;
+    GPIOC->OSPEEDR |= 0x2A800;
 }
 
 void GPIO_init_AF(){
@@ -70,7 +73,7 @@ void PWM_channel_init(){
     //110: PWM mode 1: TIMx_CNT<TIMx_CCR2-->active, or inactive
     TIM2->CCMR1 |= (TIM_CCMR1_OC2M_1 | TIM_CCMR1_OC2M_2);
 
-    TIM2->CCR2 = 50;
+    TIM2->CCR2 = 0;
 }
 
 char keypad_scan(){
@@ -83,6 +86,7 @@ char keypad_scan(){
     // int total_pressed_value = 0;
 
     while(1){
+        int ispressed = 0;
         for(key_row = 0; key_row < 4; key_row++){
             for(key_col = 0; key_col < 4; key_col++){
                 // send 0b1 to target column pin(PC5-PC8)
@@ -94,13 +98,19 @@ char keypad_scan(){
 
                 // current key is pressed
                 if(row_output != 0){
-                    if(key_value[key_row][key_col] != 0){
-                        TIM2->PSC = 152U;
+                    ispressed = 1;
+                    if(key_value[key_row][key_col]){
+                        TIM2->PSC = 4000000 / (key_value[key_row][key_col] * 10);
                         TIM2->CCR2 = 50;
-                        TIM2->CR1 |= TIM_CR1_CEN;
+                    }
+                    else{
+                        TIM2->CCR2 = 0;
                     }
                 }
             }
+        }
+        if(!ispressed){
+            TIM2->CCR2 = 0;
         }
     }
 }
@@ -111,6 +121,7 @@ int main(){
     GPIO_init_AF();
     Timer_init();
     PWM_channel_init();
+    TIM2->CR1 |= TIM_CR1_CEN;
     //TODO: Scan the keypad and use PWM to send the corresponding frequency square wave to buzzer.
     keypad_scan();
 }
