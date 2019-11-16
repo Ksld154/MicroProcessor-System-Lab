053be7494cb7de36d626daaba5c3864c452ffff5
Nicholas Li <43367240+rti56kt@users.noreply.github.com>
2019-11-16 16:03:25 +0800

    a test file

---
+++ Lab6/P3.c
@@ -0,0 +1,116 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "stm32l476xx.h"
+
+#define PAD_SIZE 4
+
+extern void GPIO_init();
+
+// float key_value[PAD_SIZE][PAD_SIZE] = {
+//     {261.6,  293.7,  329.6,  0},
+//     {349.2,  392.0,  440.0,  0},
+//     {493.9,  523.3,      0,  0},
+//     {    0,      0,      0,  0}
+// };
+
+int key_value[PAD_SIZE][PAD_SIZE] = {
+    {261,  293,  329,  0},
+    {349,  392,  440,  0},
+    {493,  523,    0,  0},
+    {  0,    0,    0,  0}
+};
+
+void keypad_init(){
+    // using GPIO_A, GPIO_B, GPIO_C
+    RCC->AHB2ENR = 0x7;
+
+    // PB5-PB8: input mode
+    // Set PB5-PB8 to 0b00 00 00 00 (input mode)
+    GPIOB->MODER = 0xFFFC03FF;
+
+    // Set PB5-PB8 to 0b01 01 01 01 (pull-down)
+    GPIOB->PUPDR = 0xFFFC03FF;
+    GPIOB->PUPDR = GPIOB->PUPDR | 0x2A800;
+
+    // Set PB5-PB8 to 0b10 10 10 10 (high speed)
+    GPIOB->OSPEEDR = 0xFFF2A8FF;
+
+
+    // PC5-PC8: output mode
+    // Set PC5-PC8 to 0b01 01 01 01 (output mode)
+    GPIOC->MODER = 0xFFFC03FF;
+    GPIOC->MODER = GPIOC->MODER | 0x15400;
+
+    // Set PC5-PC8 to 0b10 10 10 10 (high speed)
+    GPIOC->OSPEEDR = 0xFFF2A8FF;
+}
+
+void GPIO_init_AF(){
+    //TODO: Initial GPIO pin as alternate function for buzzer. You can choose to use C or assembly to finish this function.
+    GPIOB->AFR[0] &= ~(GPIO_AFRL_AFSEL3);//AFR[0] LOW
+    GPIOB->AFR[0] |= GPIO_AFRL_AFSEL3_0;//PB3
+}
+
+void Timer_init(){
+    //TODO: Initialize timer
+    RCC->APB1ENR1 |= RCC_APB1ENR1_TIM2EN;
+    TIM2->CR1 &= ~(TIM_CR1_CEN);
+    TIM2->PSC = 39999U;
+    TIM2->ARR = 99U;
+    TIM2->EGR = TIM_EGR_UG;
+}
+
+void PWM_channel_init(){
+    //TODO: Initialize timer PWM channel
+    TIM2->CR1 |= TIM_CR1_ARPE;
+    //enable output compare
+    TIM2->CCER |= TIM_CCER_CC2E;
+
+    TIM2->CCMR1 &= ~(TIM_CCMR1_OC2M);
+    //110: PWM mode 1: TIMx_CNT<TIMx_CCR2-->active, or inactive
+    TIM2->CCMR1 |= (TIM_CCMR1_OC2M_1 | TIM_CCMR1_OC2M_2);
+
+    TIM2->CCR2 = 50;
+}
+
+char keypad_scan(){
+    // PC5-PC8: keypad input  X (col0-col3)
+    // PB5-PB8: keypad output Y (row0-row3)
+
+    int key_row = 0;
+    int key_col = 0;
+
+    // int total_pressed_value = 0;
+
+    while(1){
+        for(key_row = 0; key_row < 4; key_row++){
+            for(key_col = 0; key_col < 4; key_col++){
+                // send 0b1 to target column pin(PC5-PC8)
+                GPIOC->BRR = 0xF << 5;
+                GPIOC->BSRR = (0x1 << (key_col+5));
+
+                // get keypad output value
+                int row_output = ((GPIOB->IDR) >> (key_row+5)) & 0x1;    // last 4 bit represent PB5-PB8's input
+
+                // current key is pressed
+                if(row_output != 0){
+                    if(key_value[key_row][key_col] != 0){
+                        TIM2->PSC = 152U;
+                        TIM2->CCR2 = 50;
+                        TIM2->CR1 |= TIM_CR1_CEN;
+                    }
+                }
+            }
+        }
+    }
+}
+
+int main(){
+    GPIO_init();
+    keypad_init();
+    GPIO_init_AF();
+    Timer_init();
+    PWM_channel_init();
+    //TODO: Scan the keypad and use PWM to send the corresponding frequency square wave to buzzer.
+    keypad_scan();
+}
